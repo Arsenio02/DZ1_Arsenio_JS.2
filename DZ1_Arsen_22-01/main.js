d1428d8c96c8c3765249751a15e7cf1c8c243581
Arsenio02 <128214234+Arsenio02@users.noreply.github.com>
2023-05-27 04:07:48 +0600

    Add files via upload

---
+++ DZ1_Arsen_22-01/main.js
@@ -0,0 +1,38 @@
+// First
+
+// function validateEmail() {
+//     let email = document.getElementById("email").value;
+//     let regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+//     if (!regex.test(email)) {
+//         alert("Invalid email address");
+//     }
+// }
+
+
+// Second
+
+// const field = document.getElementById("field");
+// const block = document.getElementById("block");
+//
+// let x = 0;
+// let y = 0;
+//
+// const moveBlock = () => {
+//     x += 10;
+//     y += 10;
+//
+//     if (x > field.offsetWidth - block.offsetWidth) {
+//         x = 0;
+//     }
+//
+//     if (y > field.offsetHeight - block.offsetHeight) {
+//         y = 0;
+//     }
+//
+//     block.style.left = x + "px";
+//     block.style.right = y + "px";
+//
+//     setTimeout(moveBlock, 50);
+// };
+//
+// moveBlock();
